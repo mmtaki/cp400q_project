9926c1d4d7e81ea16fecdb5d5bb8bceda8897ef4
mmtaki <moha7220@mylaurier.ca>
2014-01-15 12:47:31 -0500

    Updated bluetooth app

---
+++ BluetoothTest/src/com/chaumoha/MainApplication.java
@@ -0,0 +1,44 @@
+package com.chaumoha;
+
+import java.util.Set;
+
+import android.app.Application;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
+import android.util.Log;
+
+import com.chaumoha.btxfr.ClientThread;
+import com.chaumoha.btxfr.ProgressData;
+import com.chaumoha.btxfr.ServerThread;
+
+public class MainApplication extends Application {
+    private static String TAG = "BTPHOTO/MainApplication";
+    protected static BluetoothAdapter adapter;
+    protected static Set<BluetoothDevice> pairedDevices;
+    protected static Handler clientHandler;
+    protected static Handler serverHandler;
+    protected static ClientThread clientThread;
+    protected static ServerThread serverThread;
+    protected static ProgressData progressData = new ProgressData();
+
+    protected static final String TEMP_IMAGE_FILE_NAME = "btimage.jpg";
+    protected static final int PICTURE_RESULT_CODE = 1234;
+    protected static final int IMAGE_QUALITY = 100;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter != null) {
+            if (adapter.isEnabled()) {
+                pairedDevices = adapter.getBondedDevices();
+            } else {
+                Log.e(TAG, "Bluetooth is not enabled");
+            }
+        } else {
+            Log.e(TAG, "Bluetooth is not supported on this device");
+        }
+    }
+}
---
+++ BluetoothTest/src/com/chaumoha/btxfr/ProgressData.java
@@ -0,0 +1,6 @@
+package com.chaumoha.btxfr;
+
+public class ProgressData {
+    public long totalSize = 0;
+    public long remainingSize = 0;
+}
