8cb06e69cd90b02a0f75485fe5d8b91ff04deb6b
Bryan Chau <chau3120@mylaurier.ca>
2014-01-26 14:16:38 -0500

    Started on a02, got lazy. SelfReminder: PAGE 98 of text

---
+++ cp400q_chaumoha_a02/src/com/chaumoha/a01/CheatActivity.java
@@ -0,0 +1,13 @@
+package com.chaumoha.a01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+}
