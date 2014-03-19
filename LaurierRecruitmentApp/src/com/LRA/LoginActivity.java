44d546224d78f03c06139b19fc9e63ea38c74b2b
Bryan Chau <chau3120@mylaurier.ca>
2014-03-19 18:32:49 -0400

    Start login process

---
+++ LaurierRecruitmentApp/src/com/LRA/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.LRA;
+
+import com.example.LRA.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+
+public class LoginActivity extends FragmentActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		FragmentManager manager = this.getSupportFragmentManager();
+		Fragment fragment = manager.findFragmentById(R.id.login_container);
+		
+		if (fragment == null){
+			fragment = new LoginFragment();
+			
+			manager.beginTransaction()
+			.add(R.id.login_container, fragment)
+            .commit();
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+}
---
+++ LaurierRecruitmentApp/src/com/LRA/LoginFragment.java
@@ -0,0 +1,72 @@
+package com.LRA;
+
+import com.example.LRA.R;
+import com.example.LRA.R.id;
+import com.example.LRA.R.layout;
+import com.example.LRA.R.menu;
+import com.example.LRA.R.string;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class LoginFragment extends Fragment {
+	/**
+	 * A dummy authentication store containing known user names and passwords.
+	 * TODO: remove after connecting to a real authentication system.
+	 */
+	private static final String[] DUMMY_CREDENTIALS = new String[] {
+			"foo@example.com:hello", "bar@example.com:world" };
+
+	/**
+	 * The default email to populate the email field with.
+	 */
+	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	//private UserLoginTask mAuthTask = null;
+
+	// Values for email and password at the time of the login attempt.
+	private String mEmail;
+	private String mPassword;
+
+	// UI references.
+	private EditText mEmailView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+	
+	public static LoginFragment newInstance(){
+		return new LoginFragment();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+	}
+		
+	
+
+
+}
