7c52c24cb327c4bf0d190f39ab3b3740d66428ad
Bryan Chau <chau3120@mylaurier.ca>
2014-03-20 15:25:54 -0400

    Dabble with login page

--- LaurierRecruitmentApp/gen/com/example/LRA/R.java
+++ LaurierRecruitmentApp/gen/com/example/LRA/R.java
@@ -21,23 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int com_facebook_logo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000d;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_forgot_password=0x7f08000f;
+        public static final int action_settings=0x7f08000e;
         public static final int email=0x7f080004;
         public static final int fragment_container=0x7f080000;
-        public static final int gridRowImageView=0x7f080008;
-        public static final int gridRowTextView=0x7f080009;
+        public static final int gridRowImageView=0x7f08000a;
+        public static final int gridRowTextView=0x7f08000b;
         public static final int gridview=0x7f080001;
         public static final int login=0x7f080006;
         public static final int login_container=0x7f080002;
         public static final int login_form=0x7f080003;
-        public static final int login_status=0x7f08000a;
-        public static final int login_status_message=0x7f08000b;
+        public static final int login_status=0x7f08000c;
+        public static final int login_status_message=0x7f08000d;
         public static final int password=0x7f080005;
         public static final int sign_in_button=0x7f080007;
+        public static final int sign_in_with_facebook_button=0x7f080009;
+        public static final int sign_up_button=0x7f080008;
     }
     public static final class layout {
         public static final int activity_grid_menu=0x7f030000;
@@ -50,23 +53,25 @@ public final class R {
         public static final int login=0x7f070001;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f050009;
+        public static final int action_forgot_password=0x7f05000b;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050007;
-        public static final int action_sign_in_short=0x7f050008;
+        public static final int action_sign_in=0x7f050008;
+        public static final int action_sign_in_short=0x7f05000a;
+        public static final int action_sign_up=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f05000e;
-        public static final int error_incorrect_password=0x7f05000d;
-        public static final int error_invalid_email=0x7f05000b;
-        public static final int error_invalid_password=0x7f05000c;
+        public static final int error_field_required=0x7f050010;
+        public static final int error_incorrect_password=0x7f05000f;
+        public static final int error_invalid_email=0x7f05000d;
+        public static final int error_invalid_password=0x7f05000e;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000a;
+        public static final int login_progress_signing_in=0x7f05000c;
+        public static final int or_sign_in_text=0x7f050004;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050005;
-        public static final int prompt_password=0x7f050006;
+        public static final int prompt_email=0x7f050006;
+        public static final int prompt_password=0x7f050007;
         public static final int title_activity_grid_menu_fragment=0x7f050003;
-        public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_login=0x7f050005;
     }
     public static final class style {
         /** 
